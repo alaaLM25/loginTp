d6d689197bf052f1cca59a49fdf2e58d898d0211
alaaLM25 <alalimem25@gmail.com>
2023-11-11 11:56:09 +0100

    Add login ui && logic

--- app/src/main/java/com/example/login/Login.kt
+++ app/src/main/java/com/example/login/Login.kt
@@ -1,48 +1,48 @@
 package com.example.login
 
-import android.R.attr.data
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 import com.google.firebase.auth.FirebaseAuth
 
+class Login : AppCompatActivity() {
 
-class Register : AppCompatActivity() {
 
     lateinit var btn: Button
     lateinit var email: TextInputEditText
     lateinit var password: TextInputEditText
-    lateinit var toLogin: TextView
-    lateinit var auth :FirebaseAuth
+    lateinit var register: TextView
+    lateinit var auth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        setContentView(R.layout.activity_login)
 
-        btn = findViewById(R.id.btn_reg)
+        btn = findViewById(R.id.btn_log)
         email = findViewById(R.id.email)
         password = findViewById(R.id.pws)
-        toLogin = findViewById(R.id.to_log)
+        register = findViewById(R.id.to_reg)
         auth = FirebaseAuth.getInstance()
 
 
-        toLogin.setOnClickListener {
 
-            val intent = Intent(this, Login::class.java)
+
+        register.setOnClickListener {
+
+            val intent = Intent(this, Register::class.java)
             startActivity(intent)
-
 
         }
 
 
+
+
         btn.setOnClickListener {
-
-
             if (email.text.toString().isEmpty()) {
                 Toast.makeText(
                     this, "email is empty",
@@ -60,21 +60,16 @@ class Register : AppCompatActivity() {
                 return@setOnClickListener
             }
 
-             val emailTosend = email.text.toString()
-             val pswToSend = password.text.toString()
 
-            auth.createUserWithEmailAndPassword(emailTosend, pswToSend)
+            val emailTosend = email.text.toString()
+            val pswToSend = password.text.toString()
+
+            auth.signInWithEmailAndPassword(emailTosend, pswToSend)
                 .addOnCompleteListener(this) { task ->
                     if (task.isSuccessful) {
                         // Sign in success, update UI with the signed-in user's information
-
-                        Toast.makeText(
-                            baseContext,
-                            "Compte is created.",
-                            Toast.LENGTH_SHORT,
-                        ).show()
-
-
+                        val intent = Intent(this, MainActivity::class.java)
+                        startActivity(intent)
 
                     } else {
                         // If sign in fails, display a message to the user.
@@ -83,17 +78,10 @@ class Register : AppCompatActivity() {
                             "Authentication failed.",
                             Toast.LENGTH_SHORT,
                         ).show()
+
                     }
                 }
-
-
-
-
-
-
         }
-
-
     }
 
 
